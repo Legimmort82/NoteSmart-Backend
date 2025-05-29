6649a73156942f45a1e1590e0fccfff98bbf368e
Legimmort82 <legendlegendery1212@gmail.com>
2025-05-29 12:34:00 +0330

    fixed

--- Database/db.js
+++ Database/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectToDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/NoteApp");
+    await mongoose.connect("mongodb+srv://salimmh8282:Salim.1382@legimmort.smtwg9p.mongodb.net/NoteApp?retryWrites=true&w=majority&appName=Legimmort");
     console.log("connected successfully");
   } catch (error) {
     console.log(error);
