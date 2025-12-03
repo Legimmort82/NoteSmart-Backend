aa054894cd8fc2160a2a41e1c2ca7bb6c093de28
Legimmort <legendlegendery1212@gmail.com>
2025-12-03 11:37:00 +0330

    edited url

--- Database/db.ts
+++ Database/db.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectToDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017");
+    await mongoose.connect("mongodb://localhost:27017/NoteApp");
     console.log("connected successfully");
   } catch (error) {
     console.log(error);
