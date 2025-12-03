93eab85b67e84a1ce9818c479917e2b6df3fa822
Legimmort <legendlegendery1212@gmail.com>
2025-12-03 11:36:31 +0330

    edited mongo url

--- Database/db.ts
+++ Database/db.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectToDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://salimmh8282:Salim.1382@legimmort.smtwg9p.mongodb.net/NoteApp?retryWrites=true&w=majority&appName=Legimmort");
+    await mongoose.connect("mongodb://localhost:27017");
     console.log("connected successfully");
   } catch (error) {
     console.log(error);
